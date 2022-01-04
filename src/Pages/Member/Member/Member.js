b85d5a8171129fbdb74a496bf676fbc7267d3196
AnirbanAnu2000 <anirbananu2000@gmail.com>
2022-01-04 21:39:47 +0600

    last commit

--- src/Pages/Member/Member/Member.js
+++ src/Pages/Member/Member/Member.js
@@ -10,11 +10,9 @@ const Member = (props) => {
       <Card style={{borderRadius:"10px"}} className="product-card p-2">
         <Card.Img className="product-img" variant="top" src={img} style={{height:"350px",borderTopLeftRadius:"10px",borderTopRightRadius:"10px"}} />
         <Card.Body>
-          <Card.Title><h4 style={{fontSize:"20px"}} className="brand-color fw-bold">Book: {name}</h4></Card.Title>
-          <Card.Title><h5 style={{fontSize:"20px"}} className="brand-color fw-bold">Author: {address}</h5></Card.Title>
-          <Card.Text>
-            <h5 className="fw-bold">Publishing Year: {contact}</h5>
-          </Card.Text>
+          <Card.Title><h4 style={{fontSize:"20px"}} className="brand-color fw-bold">Member: {name}</h4></Card.Title>
+          <Card.Title><h5 style={{fontSize:"20px"}} className="brand-color fw-bold">Address: {address}</h5></Card.Title>
+          <Card.Text><h5 className="fw-bold">Contact Number: {contact}</h5></Card.Text>
         </Card.Body>  
       </Card>
     </Col>
